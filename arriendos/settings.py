6a91aa778704f3738bd2cd1940a74831ca1bfa23
luispablom <luispablomatamalabriones>
2024-09-01 18:11:40 -0400

    [ADD] Se configuro la base de datos postgres y variables de entorno

--- arriendos/settings.py
+++ arriendos/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +84,12 @@ WSGI_APPLICATION = 'arriendos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'arriendos',
+        'USER': db_user,
+        'PASSWORD': db_password,
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
