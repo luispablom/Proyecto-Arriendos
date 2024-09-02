0941a5d86e476e34b11acb8d81685f760b9a4b91
luispablom <luispablomatamalabriones>
2024-09-02 18:28:58 -0400

    [ADD] se agrega vista profile y la opcion de modificar los datos de usuario

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from main.views import register, index
+from main.views import register, index, profile
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('acounts/register/', register, name='register'),
+    path('accounts/register/', register, name='register'),
+    path('accounts/profile/', profile, name='profile'),
 ]
